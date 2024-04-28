b5eb8e46a32e514ba3ed47a2c148885ae558b976
Dovydas Alek. <d.alek.da@gmail.com>
2024-04-28 20:55:44 +0100

    Fix Input component to enable user input functionality

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,20 +1,19 @@
-// import {useState} from "react";
+import {useState} from "react";
 
 function Input({children, ...props}) {
-	// const [input, setInput] = useState("");
-	// const handleInputChange = (e) => {
-	// 	const {value} = e.target;
-	// 	setInput(value);
-
-	// };
+	const [input, setInput] = useState("");
+	const handleInputChange = (e) => {
+		const {value} = e.target;
+		setInput(value);
+	};
 
 	return (
 		<div>
 			<label>{children}</label>
 			<input
 				type="number"
-				// value={input}
-				// onChange={handleInputChange}
+				value={input}
+				onChange={handleInputChange}
 				{...props}
 			/>
 		</div>
